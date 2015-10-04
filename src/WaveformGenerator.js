dcba99fd1bff82b1428bc76f6fc3f6b6fcf80661
Jeremy Karlsson <hello@jeremy.se>
2015-10-04 18:18:32 +0200

    This works

--- src/WaveformGenerator.js
+++ src/WaveformGenerator.js
@@ -1,142 +1,151 @@
 var WaveformGenerator = (function(audioBuffer, settingsObject) {
-  var settings = undefined;
+    var settings = undefined;
 
-  var defaultSettings = {
-    waveformWidth: 500,
-    waveformHeight: 80,
-    waveformColor: '#bada55',
-    barAlign: 'center',
-    barWidth: 1,
-    barGap: 0,
-    drawMode: 'png'
-  };
+    var defaultSettings = {
+        waveformWidth: 500,
+        waveformHeight: 80,
+        waveformColor: '#bada55',
+        barAlign: 'center',
+        barWidth: 1,
+        barGap: 0,
+        drawMode: 'png'
+    };
 
-  var audioContext = new AudioContext() || new WebkitAudioContext();
-  var svg = null;
-  var svgStyleSheet = null;
-  var canvas = null;
+    var audioContext = new AudioContext() || new WebkitAudioContext();
+    var svg = null;
+    var svgStyleSheet = null;
+    var canvas = null;
 
-  function guid() {
-    return 'a' + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1) + 'b' + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-  }
-    
-  function drawBar(index, barHeight) {
-    var barWidth = settings.barWidth;
-
-    if (settings.barGap !== 0) {
-      barWidth *= Math.abs(1 - settings.barGap);
+    function guid() {
+        return 'a' + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1) + 'b' + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
     }
 
-    var x = index + (barWidth / 2);
-    var y;
+    function drawBar(index, barHeight) {
+        var barWidth = settings.barWidth;
 
-    switch (settings.barAlign) {
-      case 'top':
-        y = 0;
-        break;
-      case 'center':
-        y = settings.waveformHeight / 2 - barHeight / 2;
-        break;
-      case 'bottom':
-        y = settings.waveformHeight - barHeight;
-        break;
-    }
-
-    if (settings.drawMode === 'png') {
-      var ctx = canvas.getContext('2d');
-      ctx.fillStyle = settings.waveformColor;
-      ctx.fillRect(x, y, barWidth, barHeight);
-    }
-    else if (settings.drawMode === 'svg') {
-      var path = document.createElement('path');
-
-      path.setAttribute('d', 'M' + x + ' ' + y + ' L' + x + ' ' + y + ' L' + x + ' ' + (y + barHeight) + ' L' + x + ' ' + (y + barHeight) + ' L' + x + ' ' + y + ' Z');
-      path.className = svg.id;
-
-      svg.appendChild(path);
-    }
-  }
-
-  function bufferMeasure(position, length, data) {
-    var sum = 0.0;
-    for (var i = position, ref = (position + length) - 1; position <= ref ? i <= ref : i >= ref; position <= ref ? i++ : i--) {
-      sum += Math.pow(data[i], 2);
-    }
-    return Math.sqrt(sum / data.length);
-  }
-
-  function extractBuffer(buffer) {
-    return new Promise(function(resolve, reject) {
-      buffer = buffer.getChannelData(0);
-      var waveformWidth = settings.waveformWidth;
-      var waveformHeight = settings.waveformHeight;
-      var length = Math.floor(buffer.length / waveformWidth);
-
-      var vals = [];
-
-      for (let i = 0; i < waveformWidth; i += settings.barWidth) {
-        vals.push(bufferMeasure(i * length, length, buffer));
-      }
-
-      var scale = waveformHeight / Math.max.apply(null, vals);
-
-      for (let j = 0; j < vals.length; j++) {
-        drawBar(j, vals[j] * scale);
-      }
-
-      resolve();
-    });
-  }
-
-  // Constructor
-  function WaveformGenerator(fileArrayBuffer, settingsObject) {
-    return new Promise(function(resolve, reject) {
-      audioContext.decodeAudioData(fileArrayBuffer, function(audioBuffer) {
-        if (!audioBuffer) {
-          reject(new Error('Could not decode audio data'));
-          return;
+        if (settings.barGap !== 0) {
+            barWidth *= Math.abs(1 - settings.barGap);
         }
 
-        settings = Object.assign({}, defaultSettings);
-        settings = Object.assign(settings, settingsObject);
+        var x = index + (barWidth / 2);
+        var y;
 
-        var processId = guid();
+        switch (settings.barAlign) {
+            case 'top':
+                y = 0;
+                break;
+            case 'center':
+                y = settings.waveformHeight / 2 - barHeight / 2;
+                break;
+            case 'bottom':
+                y = settings.waveformHeight - barHeight;
+                break;
+        }
 
-        svg = document.createElement('svg');
-        svg.id = processId;
-        svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-        svg.setAttribute('version', '1.1');
-        svg.setAttributeNS(null, 'viewBox', '0 0 ' + settings.waveformWidth + ' ' + settings.waveformHeight);
+        if (settings.drawMode === 'png') {
+            var ctx = canvas.getContext('2d');
+            ctx.fillStyle = settings.waveformColor;
+            ctx.fillRect(x, y, barWidth, barHeight);
+        } else if (settings.drawMode === 'svg') {
+            var path = document.createElement('path');
 
-        svgStyleSheet = document.createElement('style');
-        svgStyleSheet.setAttribute('type', 'text/css');
-        svgStyleSheet.appendChild(document.createTextNode('<![CDATA[path.'+svg.id+'{stroke:' + settings.waveformColor + ';stroke-width:' + ((settings.barGap !== 0) ? (settings.barWidth * Math.abs(1 - settings.barGap)) : settings.barWidth) + '}]]>'));
+            path.setAttribute('d', 'M' + x + ' ' + y + ' L' + x + ' ' + y + ' L' + x + ' ' + (y + barHeight) + ' L' + x + ' ' + (y + barHeight) + ' L' + x + ' ' + y + ' Z');
+            path.className = svg.id;
 
-        svg.appendChild(svgStyleSheet);
+            svg.appendChild(path);
+        }
+    }
 
-        canvas = document.createElement('canvas');
-        canvas.id = processId;
-        canvas.width = settings.waveformWidth;
-        canvas.height = settings.waveformHeight;
-        var ctx = canvas.getContext('2d');
-        ctx.clearRect(0, 0, canvas.width, canvas.height );
+    function bufferMeasure(position, length, data) {
+        var sum = 0.0;
+        for (var i = position, ref = (position + length) - 1; position <= ref ? i <= ref : i >= ref; position <= ref ? i++ : i--) {
+            sum += Math.pow(data[i], 2);
+        }
+        return Math.sqrt(sum / data.length);
+    }
 
-        extractBuffer(audioBuffer).then(function() {
-          if (settings.drawMode === 'svg') {
-            var svgUrl = URL.createObjectURL(new Blob(['<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">' + svg.outerHTML], {
-              type: 'image/svg+xml'
-            }));
-            console.log('Resolving ' + processId);
-            resolve(svgUrl);
-          } else if (settings.drawMode === 'png') {
-            console.log('Resolving ' + processId);
-            console.log('Resolving canvas #' + canvas.id);
-            resolve(canvas.toDataURL());
-          }
+    function extractBuffer(buffer) {
+        return new Promise(function(resolve, reject) {
+            buffer = buffer.getChannelData(0);
+            var sections = settings.waveformWidth;
+            var len = Math.floor(buffer.length / sections);
+            var maxHeight = settings.waveformHeight;
+
+            var vals = [];
+
+            var i;
+            for (i = 0; i < sections; i += settings.barWidth) {
+                vals.push(bufferMeasure(i * len, len, buffer) * 10000);
+            }
+
+            var scale = maxHeight / Math.max.apply(null, vals);
+
+            for (var j = 0; j < sections; j += settings.barWidth) {
+                var val = bufferMeasure(j * len, len, buffer) * 10000;
+                val *= scale;
+                val += 1;
+                drawBar(j, val);
+            }
+
+            if (i >= sections) {
+                resolve();
+            }
+
+            if (vals.length <= 0) {
+                reject(new Error('No data to extract'));
+            }
         });
-      });
-    });
-  }
+    }
 
-  return WaveformGenerator;
+    // Constructor
+    function WaveformGenerator(fileArrayBuffer, settingsObject) {
+        return new Promise(function(resolve, reject) {
+            audioContext.decodeAudioData(fileArrayBuffer, function(audioBuffer) {
+                if (!audioBuffer) {
+                    reject(new Error('Could not decode audio data'));
+                    return;
+                }
+
+                settings = Object.assign({}, defaultSettings);
+                settings = Object.assign(settings, settingsObject);
+
+                var processId = guid();
+
+                svg = document.createElement('svg');
+                svg.id = processId;
+                svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+                svg.setAttribute('version', '1.1');
+                svg.setAttributeNS(null, 'viewBox', '0 0 ' + settings.waveformWidth + ' ' + settings.waveformHeight);
+
+                svgStyleSheet = document.createElement('style');
+                svgStyleSheet.setAttribute('type', 'text/css');
+                svgStyleSheet.appendChild(document.createTextNode('<![CDATA[path.' + svg.id + '{stroke:' + settings.waveformColor + ';stroke-width:' + ((settings.barGap !== 0) ? (settings.barWidth * Math.abs(1 - settings.barGap)) : settings.barWidth) + '}]]>'));
+
+                svg.appendChild(svgStyleSheet);
+
+                canvas = document.createElement('canvas');
+                canvas.id = processId;
+                canvas.width = settings.waveformWidth;
+                canvas.height = settings.waveformHeight;
+                var ctx = canvas.getContext('2d');
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                extractBuffer(audioBuffer).then(function() {
+                    if (settings.drawMode === 'svg') {
+                        var svgUrl = URL.createObjectURL(new Blob(['<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">' + svg.outerHTML], {
+                            type: 'image/svg+xml'
+                        }));
+                        console.log('Resolving ' + processId);
+                        resolve(svgUrl);
+                    } else if (settings.drawMode === 'png') {
+                        console.log('Resolving ' + processId);
+                        console.log('Resolving canvas #' + canvas.id);
+                        resolve(canvas.toDataURL());
+                    }
+                });
+            });
+        });
+    }
+
+    return WaveformGenerator;
 })();
